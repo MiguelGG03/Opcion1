a0d6b0719951c9c450a63ff5abdfe861451f2051
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-12-09 20:14:16 +0100

    no funciona uwu

--- data_cleaner_ej1.py
+++ data_cleaner_ej1.py
@@ -2,19 +2,23 @@
 
 f = open("auto-mpg.data", mode='r',encoding='utf-8')
 lista_con_datos = []
+
 for i in f:
     lista_con_datos.append(i.split(sep=' '))
+
 for letter in lista_con_datos:
     for i in letter:
         if i == '':
             letter.remove(i)
+       
 for i in lista_con_datos:
     if i == []:
         lista_con_datos.remove(i)
 
 for i in range(len(lista_con_datos)):
-    for j in range(len(lista_con_datos[i])):
-        lista_con_datos[i][j] = lista_con_datos[i][j].replace('?', '0')
+    for j in lista_con_datos[i]:
+        if j=='':
+            lista_con_datos[i].remove(j)
 
 
 
