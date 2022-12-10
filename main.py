1dc1d83d41ca769a9a5de83db08d75b89e381ef9
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-12-10 19:33:41 +0100

    Todo el main hecho falta hacer los del 5.2

--- main.py
+++ main.py
@@ -32,3 +32,36 @@ def main():
         else:
             print('No es una opcion valida')
             main()
+    elif pr1=='2':
+        pr2=input('Que apartado desea ver?:\n'
+        '1. Apartado 5.2.1\n'
+        '2. Apartado 5.2.2\n'
+        '3. Apartado 5.2.3\n'
+        '4. Apartado 5.2.4\n'
+        '5. Apartado 5.2.5\n'
+        '6. Apartado 5.2.6\n'
+        '7. Apartado 5.2.7\n'
+        '>>> ')
+        if pr2=='1':
+            main521()
+        elif pr2=='2':
+            main522()
+        elif pr2=='3':
+            main523()
+        elif pr2=='4':
+            main524()
+        elif pr2=='5':
+            main525()
+        elif pr2=='6':
+            main526()
+        elif pr2=='7':
+            main527()
+        else:
+            print('No es una opcion valida')
+            main()
+    else:
+        print('No es una opcion valida')
+        main()
+
+if __name__ == '__main__':
+    main()
