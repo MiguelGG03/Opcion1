408338fab114cd3a213f776acc7ba4080a672dbd
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-12-10 12:43:48 +0100

    datos limpiados correctamente

--- data_cleaner_ej1.py
+++ data_cleaner_ej1.py
@@ -7,33 +7,23 @@ for i in f:
     
     var=i.replace('\t',' ')
     var2=var.replace('\n','')
-    lista_con_datos.append(var2.split(sep=' '))
+    var3=var2[0:7].strip()
+    var4=var2[7:11].strip()
+    var5=var2[11:22].strip()
+    var6=var2[22:33].strip().replace('?','0.0')
+    var7=var2[33:44].strip().replace('.','')
+    var8=var2[44:51].strip()
+    var9=var2[51:55].strip()
+    var10=var2[55:57].strip()
+    var11=var2[57:].strip().replace('"','')
+
+    lista_con_datos.append([float(var3),int(var4),float(var5),float(var6),int(var7),float(var8),int(var9),int(var10),var11])
 
 
 for letter in lista_con_datos:
     for i in letter:
         if i == '':
             letter.remove(i)
-'''       
-for i in lista_con_datos:
-    if i == []:
-        lista_con_datos.remove(i)
-
-for i in range(len(lista_con_datos)):
-    for j in lista_con_datos[i]:
-        if j=='':
-            lista_con_datos[i].remove(j)
-
-for i in range(len(lista_con_datos)):
-    for j in lista_con_datos[i]:
-        if j=='':
-            lista_con_datos[i].remove(j)
-
-for i in range(len(lista_con_datos)):
-    for j in lista_con_datos[i]:
-        if j=='':
-            lista_con_datos[i].remove(j)
-'''
 
 
 print(lista_con_datos)
