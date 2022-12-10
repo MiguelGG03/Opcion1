75d589e60494237b45824f849e10b54fe3a162fc
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-12-10 12:21:06 +0100

    Replaceando cosas

--- data_cleaner_ej1.py
+++ data_cleaner_ej1.py
@@ -4,13 +4,17 @@ f = open("auto-mpg.data", mode='r',encoding='utf-8')
 lista_con_datos = []
 
 for i in f:
-    lista_con_datos.append(i.split(sep=' '))
+    
+    var=i.replace('\t',' ')
+    var2=var.replace('\n','')
+    lista_con_datos.append(var2.split(sep=' '))
+
 
 for letter in lista_con_datos:
     for i in letter:
         if i == '':
             letter.remove(i)
-       
+'''       
 for i in lista_con_datos:
     if i == []:
         lista_con_datos.remove(i)
@@ -29,7 +33,7 @@ for i in range(len(lista_con_datos)):
     for j in lista_con_datos[i]:
         if j=='':
             lista_con_datos[i].remove(j)
-
+'''
 
 
 print(lista_con_datos)
