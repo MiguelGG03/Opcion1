f44c03b7f5cb439e94da9cbb701d5ef54fffb368
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-12-13 16:17:20 +0100

    me va el pyreadr, quiero llorar de alegria ruben

--- data_cleaners/data_cleaner_ej2.py
+++ data_cleaners/data_cleaner_ej2.py
@@ -1,8 +1,9 @@
+import pyreadr
+import pandas as pd
 
 
-
-
-
-f = open("data/tips.rda", mode='r',encoding='latin1')
-for i in f:
+data= pyreadr.read_r('data/tips.rda')
+for i in data:
     print(i)
+    print(type(data[i]))
+    print(data[i].head(10))
